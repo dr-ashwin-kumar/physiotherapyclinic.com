257e9cf1778de7c58a05867682aa36285ce1b0dc
dr-ashwin-kumar <drashwinkumar09@gmail.com>
2025-07-18 22:39:42 +0530

    Updated

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sri Sai Balaji Physiotherapy Clinic</title>
+    <link rel="icon" href="./assets/images/favicon.png" type="x-image/icon">
+
+    <!-- --------------link for the styling index page--------------- -->
+    <link rel="stylesheet" href="./style.css">
+
+    <!-- --------------link for the font awesome icons--------------- -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+
+    <!-- --------------start of the topbar--------------- -->
+    <?php include './topbar.php' ?>
+    
+</body>
+</html>
---
+++ topbar.php
@@ -0,0 +1,9 @@
+<div class="topbar">
+    <div class="topbar-mail-address">
+        <a href="#"><span><i class="fa-solid fa-envelope"></i></span>&nbsp;&nbsp;srisaibalajiclinic@gmail.com</a>
+        <a href="#"><span><i class="fa-solid fa-location-dot"></i></span>&nbsp;&nbsp;Thindlu, Bengaluru 560097</a>
+    </div>
+    <div class="topbar-social-media">
+
+    </div>
+</div>
