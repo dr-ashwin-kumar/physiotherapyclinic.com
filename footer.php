89cd3804092b9d60129a762c1aa4ebbc116cd8f1
Ashwin5641 <tic1@hhvthermaltech.com>
2025-08-12 11:44:21 +0530

    Updated

--- footer.php
+++ footer.php
@@ -19,11 +19,11 @@
         <div class="footer-content">
             <div class="footer-content-contact">
                 <h3>Contact Us</h3>
-                <p><i class="fa-solid fa-location-dot"></i>&nbsp;&nbsp;Thindlu, Bengaluru 560097</p>
+                <p><i class="fa-solid fa-location-dot"></i><span>Thindlu, Bengaluru 560097</span></p>
                 <div class="footer-contact-map">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3886.4957978454386!2d77.56361027489685!3d13.06773381276367!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae192620bac1f9%3A0x943d7105c31d87b!2sSri%20Sai%20Balaji%20Physiotherapy%20Clinic-%20Advanced%20Physiotherapy%20center!5e0!3m2!1sen!2sin!4v1753694330808!5m2!1sen!2sin" style="border:0;" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
-                <p><i class="fa-solid fa-phone-flip"></i>&nbsp;&nbsp;+91 96633 14071</p>
+                <p><i class="fa-solid fa-phone-flip"></i><span>+91 96633 14071</span></p>
             </div>
             <div class="footer-content-page-links">
                 <h3>Page Llinks</h3>
