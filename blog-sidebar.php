acaeb835edcc8910a0d35165eab0dbccd150adfb
Ashwin5641 <tic1@hhvthermaltech.com>
2025-09-11 13:45:57 +0530

    Updated

--- blog-sidebar.php
+++ blog-sidebar.php
@@ -5,4 +5,82 @@
             
         </div>
     </div>
+    <div class="blog-sidebar-banner-title">
+        <h2>Dr. Shashidar</h2>
+        <small>Physiotherapist</small>
+    </div>
+    <div class="blog-sidebar-banner-info">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+    </div>
+    <div class="blog-sidebar-banner-social-media">
+        <a href="#"><span><i class="fa-brands fa-facebook-f"></i></span></a>
+        <a href="#"><span><i class="fa-brands fa-x-twitter"></i></span></a>
+        <a href="#"><span><i class="fa-brands fa-instagram"></i></span></a>
+        <a href="#"><span><i class="fa-brands fa-youtube"></i></span></a>
+    </div>
+</div>
+
+
+<!-- -------------start of the blog our categories----------------- -->
+
+<div class="blog-sidebar-our-categories">
+    <h2>Our Categories</h2>
+    <ul>
+        <li><a href="#"><span>Back Pain</span><span>( 1 )</span></a></li>
+        <li><a href="#"><span>Knee Pain</span><span>( 1 )</span></a></li>
+        <li><a href="#"><span>Leg Pain</span><span>( 1 )</span></a></li>
+        <li><a href="#"><span>Therapy</span><span>( 1 )</span></a></li>
+    </ul>
+</div>
+
+
+<!-- ------------start of the blog recent post-------------- -->
+
+<div class="blog-sidebar-recent-post">
+    <h2>Recent Post</h2>
+    <div class="blog-sidebar-recent-post-cards">
+        <a href="#" class="blog-sidebar-recent-post-card">
+            <div class="blog-sidebar-recent-post-card-img blog-sidebar-recent-post-card-img1">
+
+            </div>
+            <div class="blog-sidebar-recent-post-card-info">
+                <small>March 8, 2025</small>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+            </div>
+        </a>
+        <a href="#" class="blog-sidebar-recent-post-card">
+            <div class="blog-sidebar-recent-post-card-img blog-sidebar-recent-post-card-img2">
+
+            </div>
+            <div class="blog-sidebar-recent-post-card-info">
+                <small>August 11, 2025</small>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+            </div>
+        </a>
+        <a href="#" class="blog-sidebar-recent-post-card">
+            <div class="blog-sidebar-recent-post-card-img blog-sidebar-recent-post-card-img3">
+
+            </div>
+            <div class="blog-sidebar-recent-post-card-info">
+                <small>September 03, 2025</small>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+            </div>
+        </a>
+    </div>
+</div>
+
+
+<!-- -------------start of the blog popular tags--------------- -->
+
+<div class="blog-sidebar-popular-tags">
+    <h2>Popular Tags</h2>
+    <div class="blog-sidebar-tags">
+        <a href="#">Therapy</a>
+        <a href="#">Bones</a>
+        <a href="#">Knee Pain</a>
+        <a href="#">Chiropractic</a>
+        <a href="#">Physiotherapy</a>
+        <a href="#">Treatments</a>
+        <a href="#">Injuries</a>
+    </div>
 </div>
--- blog1.php
+++ blog1.php
@@ -42,7 +42,32 @@
     <!-- ----------------start of the blog content section---------------- -->
     <div class="page-content-sidebar-section">
         <div class="page-content-section">
+            <div class="page-blog-content-hero-image">
+                <div class="page-blog-content-hero-img">
 
+                </div>
+            </div>
+            <div class="page-blog-content-first-info">
+                <p>Matter worldwide the physiotherapy community is divide by name, with three different toprimary titles used physiotherapy, physical therapy and kinesiotherapy. Notwithstanding, that within the term physiotherapist, there are also language derivations such as fisio, fisicos, fysio and fiso. Finding a single word to describe the community is always going to be difficult. there are also language derivations such as fisio, fisico, fysio and fiso. Finding a single word to describe the community is always going to be difficult. World Physiotherapy is a health care profession concerned with human function and movement and maximising physical potential. wound care and electromyography.</p>
+                <p>There are also language derivations such as fisio, fisico, fysio and fiso. Finding a single word to describe the community to be difficult. Concerned with human function and movement and maximising physical potential.</p>
+            </div>
+            <div class="page-blog-content-second-info">
+                <h2>Modern Strategy for Your Security</h2>
+                <p>Physicians like Hippocrates and later Galen are believed to have been the first practitioners of physical therapy, advocating massage, manual therapy techniques and hydrotherapy to treat people in 460 BC. Development of orthopedics century, the Gymnasticon exercise of the joints, similar physical therapy.</p>
+                <div class="page-blog-content-second-info-images">
+                    <div class="page-blog-content-second-info-image page-blog-content-second-info-image1"></div>
+                    <div class="page-blog-content-second-info-image page-blog-content-second-info-image2"></div>
+                </div>
+                <div class="page-blog-content-second-points">
+                    <ul>
+                        <li><span><i class="fa-solid fa-circle-check"></i></span><p>Neck and back pain caused by problems in the muscles and skeleton</p></li>
+                        <li><span><i class="fa-solid fa-circle-check"></i></span><p>Pelvic issues, such as bladder and bowel problems related to childbirth.</p></li>
+                        <li><span><i class="fa-solid fa-circle-check"></i></span><p>Loss of mobility because of trauma to the brain or spine, or due to diseases such as.</p></li>
+                        <li><span><i class="fa-solid fa-circle-check"></i></span><p>Fatigue, pain, swelling, stiffness and loss of muscle strength, for example during cancer.</p></li>
+                    </ul>
+                </div>
+                <p>Capable of operating in both normal and poorly lit environments, these cameras benefit from not requiring a inbuilt infrared illuminators as they can capture clear video images extra sensitive imaging chips.</p>
+            </div>
         </div>
         <div class="page-blog-sidebar">
             <?php include './blog-sidebar.php' ?>
